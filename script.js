5d50c4a91bcf52f1587bd01a80aaf2b7fa9838fb
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-10 12:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element=document.getElementById("level");
+let level=0;
+while(element){
+	level++;
+	element=element.parentElement;
+}
+alert("The level of the element is: " +level);
